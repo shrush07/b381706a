22d400589459fee5a20909165ce43f21465c6528
shrush07 <shrupatel07@gmail.com>
2024-12-05 17:14:22 -0500

    Project-aircall

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -6,7 +6,7 @@ const ActivityFeed = ({ calls, archiveCall, archiveAll }) => {
       <button onClick={archiveAll}>Archive All</button>
       {calls.map((call) => (
         <div key={call.id} className="call">
-          <p>{call.from} → {call.to}</p>
+          <p>To:{call.from} → From:{call.to}</p>
           <p>Type: {call.call_type}</p>
           <p>Duration: {call.duration}s</p>
           <button onClick={() => archiveCall(call.id)}>Archive</button>
